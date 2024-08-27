970def0d77376baf68ba280c99e0a7012c68830c
LeTuChiBao <bao.le@discoveryloft.com>
2024-08-27 17:34:20 +0700

    update users controller testing

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -20,11 +20,17 @@ describe('UsersController', () => {
       find: (email:string)=> {
         return Promise.resolve([{id:1, email, password: 'testpass'} as User])
       },
-      // remove: (id: number)=> {},
-      // update: ()=> {}
+      remove: (id: number)=> {
+        return Promise.resolve({id, email: 'testdelete@email'}as User)
+      },
+      update: (id:number , attrs: Partial<User>)=> {
+        return Promise.resolve({id, ...attrs} as User)
+      }
     };
     fakeAuthService= {
-      // signup: ()=> {},
+      signup:(email:string, password:string)=> {
+        return Promise.resolve({id:1, email}as User);
+      },
       signIn: (email:string, password:string)=> {
         return Promise.resolve({id:1, email, password}as User);
       }
@@ -72,12 +78,62 @@ describe('UsersController', () => {
       expect(error.message).toBe('User not found')
     }
   });
+
+  it('Delete throws an error if user with given id is not found', async()=> {
+    fakeUsersService.findOne= ()=> null;
+    try {
+      await controller.deleteUser('1')
+    } catch (error) {
+      expect(error).toBeInstanceOf(NotFoundException)
+      expect(error.message).toBe('User not found')
+    }
+  });
+
+  it('delete User return user infor with the give id', async ()=> {
+    const user = await controller.deleteUser('1')
+          expect(user).toBeDefined()
+          expect(user.email).toEqual('testdelete@email')
+  })
+
+  it('Update throws an error if user with given id is not found', async()=> {
+    fakeUsersService.findOne= ()=> null;
+    try {
+      await controller.updateUser('1',{password:'test123'})
+    } catch (error) {
+      expect(error).toBeInstanceOf(NotFoundException)
+      expect(error.message).toBe('User not found')
+    }
+  });
+
+  it('Update User return new data user update with the give id and body', async ()=> {
+    const body = {password:'updatepass'}
+    const user = await controller.updateUser('1',body)
+          expect(user).toBeDefined()
+          expect(user.id).toEqual(1)
+  })
+
+
+  it('delete User return user infor with the give id', async ()=> {
+    const user = await controller.deleteUser('1')
+          expect(user).toBeDefined()
+          expect(user.email).toEqual('testdelete@email')
+  })
+
+
   it('signIn updates session object and returns user', async()=> {
     const session = {userId: null}
     const user = await controller.signIn({email: 'test@gmail.com', password: 'testpasssword'},
       session
     );
-    console.log(user)
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1)
+  })
+
+  it('signUp updates session object and returns user', async()=> {
+    const session = {userId: null}
+    const user = await controller.signIn({email: 'test@gmail.com', password: 'testpasssword'},
+      session
+    );
     expect(user.id).toEqual(1);
     expect(session.userId).toEqual(1)
   })
